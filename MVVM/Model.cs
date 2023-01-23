ca3fe53e3ef60d08699a41b19bf1f4d82989f95c
Artem Sergeichev <a.sergeichev03@mail.ru>
2023-01-23 14:59:00 +0300

    Добавьте файлы проекта.

---
+++ MVVM/Model.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    internal class Model
+    {
+        public static List<string> ariphmeticOperation = new() { "Сложение", "Вычитание", "Умножение", "Деление" };
+
+        private double _firstNumber, _secondNumber, _ariphmeticOperationPosition;
+        
+        public double FirstNumber 
+        { 
+            set { _firstNumber = value; }
+        }
+
+        public double SecondNumber
+        {
+            set { _secondNumber = value; }
+        }
+
+        public int AriphmeticOperationPosition
+        {
+            set { _ariphmeticOperationPosition = value; }
+        }
+
+
+    }
+}
